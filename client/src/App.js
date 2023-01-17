4c6b5fd4056994537756b3939ddef1a5c46a95a0
ajeetram <86039147+ajeetram@users.noreply.github.com>
2023-01-17 19:51:19 +0530

    client folder added

---
+++ client/src/App.js
@@ -0,0 +1,86 @@
+import Upload from "./artifacts/contracts/upload.sol/upload.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import FileUpload from "./Components/FileUpload";
+import Display from "./Components/Display";
+import Modal from "./Components/Modal";
+import "./App.css";
+
+function App() {
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const loadProvider = async () => {
+      if (provider) {
+        window.ethereum.on("chainChanged", () => {
+          window.location.reload();
+        });
+
+        window.ethereum.on("accountsChanged", () => {
+          window.location.reload();
+        });
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+        let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+        const contract = new ethers.Contract(
+          contractAddress,
+          Upload.abi,
+          signer
+        );
+        //console.log(contract);
+        setContract(contract);
+        setProvider(provider);
+      } else {
+        console.error("Metamask is not installed");
+      }
+    };
+    provider && loadProvider();
+  }, []);
+  return (
+    <>
+    <div className="row">
+    <div className="column1">
+      {!modalOpen && (
+        <button className="share" onClick={() => setModalOpen(true)}>
+          Share
+        </button>
+      )}
+      {modalOpen && (
+        <Modal setModalOpen={setModalOpen} contract={contract}></Modal>
+      )}
+
+      <div className="column2">
+      
+      <span>Account</span> : {account ? String(account).slice(0,10): "Not connected"}...
+    
+    </div>
+      </div>
+     
+        </div>
+      <div className="App">
+        <h1 style={{ color: "white" }}>D-Drive 3.0</h1>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+
+        
+        <FileUpload
+          account={account}
+          provider={provider}
+          contract={contract}
+        ></FileUpload>
+        <Display contract={contract} account={account}></Display>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Components/FileUpload.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import axios from "axios";
+import "./FileUpload.css";
+const FileUpload = ({ contract, account, provider }) => {
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState("No image selected");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (file) {
+      try {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        const resFile = await axios({
+          method: "post",
+          
+          url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          data: formData,
+          headers: {
+            pinata_api_key:"d95b29d6db2fd144c19b",
+            pinata_secret_api_key: "b508a096fc7edb12f153248106fa28d2f1a33fb8f322807896fbf6fa6fb84c05",
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        
+        const ImgHash = `ipfs://${resFile.data.IpfsHash}`;
+        console.log(ImgHash);
+        //const signer = contract.connect(provider.getSigner());
+        const signer = contract.connect(provider.getSigner());
+        signer.add(account, ImgHash);
+      } catch (e) {
+        alert("Unable to upload image to Pinata");
+      }
+    }
+    alert("Successfully Image Uploaded");
+    setFileName("No image selected");
+    setFile(null);
+  };
+  const retrieveFile = (e) => {
+    const data = e.target.files[0]; //files array of files object
+    // console.log(data);
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(data);
+    reader.onloadend = () => {
+      setFile(e.target.files[0]);
+    };
+    setFileName(e.target.files[0].name);
+    e.preventDefault();
+  };
+  return (
+    <div className="top">
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="file-upload" className="choose">
+          Choose Image
+        </label>
+        <input
+          disabled={!account}
+          type="file"
+          id="file-upload"
+          name="data"
+          onChange={retrieveFile}
+        />
+        <span className="textArea">Image: {fileName}</span>
+        <button type="submit" className="upload" disabled={!file}>
+          Upload File
+        </button>
+      </form>
+    </div>
+  );
+};
+export default FileUpload;
+
+
+
